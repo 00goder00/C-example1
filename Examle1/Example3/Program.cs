cd905065e958a9e617930cbf0dc4db7de5747c5b
Kirill Kaschenko <kirkasc@mail.ru>
2023-06-22 14:32:37 +0300

    add Random

--- Examle1/Example3/Program.cs
+++ Examle1/Example3/Program.cs
@@ -1,4 +1,6 @@
-﻿int numderA = 3;
-int numberB = 5;
-int result = numderA + numberB;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB ;
 Console.WriteLine(result);
